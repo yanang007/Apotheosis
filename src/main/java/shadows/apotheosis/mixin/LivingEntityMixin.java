3558fc03f09e5b6d7a359032ff364a9bdab59993
yanang007 <yanang007@gmail.com>
2022-08-15 18:29:16 +0800

    Refactor Overwrite of getDamageAfterMagicAbsorb to Redirects

--- src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
+++ src/main/java/shadows/apotheosis/mixin/LivingEntityMixin.java
@@ -1,15 +1,12 @@
 package shadows.apotheosis.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.stats.Stats;
-import net.minecraft.world.damagesource.CombatRules;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -19,7 +16,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import shadows.apotheosis.Apoth;
 
@@ -31,52 +27,30 @@ public abstract class LivingEntityMixin extends Entity {
 	}
 
 	/**
-	 * @author Shadows
-	 * @reason Injection of Sundering Potion Effect
-	 * Calculates damage taken based on potions. Required for sundering.
+	 * Enable RESISTANCE logic when either RESISTANCE and SUNDERING is present.
 	 * Called from {@link LivingEntity#getDamageAfterMagicAbsorb(DamageSource, float)}
-	 * TODO: Reduce to @Inject
 	 */
-	@Overwrite
-	public float getDamageAfterMagicAbsorb(DamageSource source, float damage) {
-		if (source.isBypassMagic()) {
-			return damage;
-		} else {
-			float mult = 1;
-			if (this.hasEffect(MobEffects.DAMAGE_RESISTANCE) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1;
-				mult -= 0.2 * level;
-			}
-			if (Apoth.Effects.SUNDERING != null && this.hasEffect(Apoth.Effects.SUNDERING) && source != DamageSource.OUT_OF_WORLD) {
-				int level = this.getEffect(Apoth.Effects.SUNDERING).getAmplifier() + 1;
-				mult += 0.2 * level;
-			}
+	@Redirect(method = "getDamageAfterMagicAbsorb", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hasEffect(Lnet/minecraft/world/effect/MobEffect;)Z", ordinal = 0))
+	public boolean getDamageAfterMagicAbsorb_hasEffect(LivingEntity instance, MobEffect effect) {
+		assert MobEffects.DAMAGE_RESISTANCE == effect; // this indicates that code related to RESISTANCE remains unchanged
+		return this.hasEffect(effect) || this.hasEffect(Apoth.Effects.SUNDERING);
+	}
 
-			float newDamage = damage * mult;
-			float resisted = damage - newDamage;
-
-			if (resisted > 0.0F && resisted < 3.4028235E37F) {
-				if ((Object) this instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_RESISTED), Math.round(resisted * 10.0F));
-				} else if (source.getEntity() instanceof ServerPlayer sp) {
-					sp.awardStat(Stats.CUSTOM.get(Stats.DAMAGE_DEALT_RESISTED), Math.round(resisted * 10.0F));
-				}
-			}
-
-			damage = newDamage;
-
-			if (damage <= 0.0F) {
-				return 0.0F;
-			} else {
-				int k = EnchantmentHelper.getDamageProtection(this.getArmorSlots(), source);
-
-				if (k > 0) {
-					damage = CombatRules.getDamageAfterMagicAbsorb(damage, k);
-				}
-
-				return damage;
-			}
+	/**
+	 * Calculate the equivalent RESISTANCE amplifier with SUNDERING considered.
+	 * Called from {@link LivingEntity#getDamageAfterMagicAbsorb(DamageSource, float)}
+	 */
+	@Redirect(method = "getDamageAfterMagicAbsorb", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectInstance;getAmplifier()I"))
+	public int getDamageAfterMagicAbsorb_getAmplifier(MobEffectInstance instance) {
+		int amplifier = 0;
+		if (this.hasEffect(MobEffects.DAMAGE_RESISTANCE)) {
+			amplifier += this.getEffect(MobEffects.DAMAGE_RESISTANCE).getAmplifier() + 1;
 		}
+		if (Apoth.Effects.SUNDERING != null && this.hasEffect(Apoth.Effects.SUNDERING)) {
+;			amplifier -= this.getEffect(Apoth.Effects.SUNDERING).getAmplifier() + 1;
+		}
+
+		return amplifier - 1; // the return value of this method added by 1, so we need to subtract 1 to get the correct value
 	}
 
 	@Shadow
